84e784418f8d5d24f032c72ebc397883daf80ed3
Frank Cheng <ufo22940268@gmail.com>
2020-09-03 06:23:31 +0800

    v

--- test/endPoint.test.ts
+++ test/endPoint.test.ts
@@ -2,29 +2,31 @@ import request from 'supertest';
 import app from '../src/app';
 import mongoose from 'mongoose';
 import { EndPoint } from '../src/models/EndPoint';
-import objectContaining = jasmine.objectContaining;
 
 
 describe('EndPoint Api', () => {
 
   beforeAll(async () => {
     await mongoose.connection.dropDatabase();
+    let endPoint = new EndPoint();
+    endPoint.url = 'u2';
+    await endPoint.save();
   });
 
   it('should upsert end point', async () => {
     await request(app).post('/endpoint/upsert')
       .send({ url: 'u1', watchFields: [{ path: 'p1', value: 'v1' }] });
-    expect(await EndPoint.countDocuments({})).toEqual(1);
-    let ep = await EndPoint.findOne();
+    expect(await EndPoint.countDocuments({ url: 'u1' })).toEqual(1);
+    let ep = await EndPoint.findOne({ url: 'u1' });
     expect(ep).toHaveProperty('url', 'u1');
     expect(ep.watchFields[0])
       .toEqual(expect.objectContaining({ path: 'p1', value: 'v1' }));
 
     await request(app).post('/endpoint/upsert')
       .send({ url: 'u1', watchFields: [{ path: 'p2', value: 'v1' }] });
-    expect(await EndPoint.countDocuments({})).toEqual(1);
-    expect(await EndPoint.findOne()).toHaveProperty('url', 'u1');
-    ep = await EndPoint.findOne();
+    expect(await EndPoint.countDocuments({ url: 'u1' })).toEqual(1);
+    expect(await EndPoint.findOne({ url: 'u1' })).toHaveProperty('url', 'u1');
+    ep = await EndPoint.findOne({ url: 'u1' });
     expect(ep.watchFields[0])
       .toEqual(expect.objectContaining({ path: 'p2', value: 'v1' }));
   });
