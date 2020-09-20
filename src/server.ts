bb95dd5968d554677b8a4b265e1094d4abdf55ae
Frank Cheng <ufo22940268@gmail.com>
2020-09-21 07:10:17 +0800

    v

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,7 @@ const server = app.listen(app.get("port"), () => {
         "  App is running at http://localhost:%d in %s mode",
         app.get("port"),
         app.get("env")
+
     );
     console.log("  Press CTRL-C to stop\n");
 });
